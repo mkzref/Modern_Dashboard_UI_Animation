99a97376107211f41b3fd27646a1de77b16d0591
mkzref <marwahatamleh2@gmail.com>
2021-03-07 18:57:45 +0200

    Changetwo

---
+++ app/src/main/java/com/example/moderndashboarduianimation/DashboardUI_2.java
@@ -0,0 +1,14 @@
+package com.example.moderndashboarduianimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardUI_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_u_i_2);
+    }
+}
